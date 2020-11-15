5b438073165c2cebc2d359056f644d771ce298bc
Yohannes2010 <abebejhon@yahoo.com>
2020-11-15 16:25:16 +0100

    first commit

---
+++ src/GameTask/Game.java
@@ -0,0 +1,21 @@
+package GameTask;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    JFrame jFrame = new JFrame("Game Play");
+    JPanel jPanelMain = new JPanel();
+    JPanel jPanel = new JPanel();
+    JPanel jPanel1 = new JPanel();
+    JButton[][] jButtons = new JButton[4][4];
+    JButton nyttSpelButton = new JButton();
+    JButton closeWindowButton = new JButton();
+    JButton finishButton = new JButton();
+    public Game(){
+
+    }
+
+    public static void main(String[] args) {
+        Game g=new Game();
+    }
+}
